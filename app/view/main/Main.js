d9c5326bf882cc7ac2265401430cb40fa9edb88f
Dominik Mikiewicz <dominik@cartomatic.pl>
2017-01-23 22:45:51 +0100

    make it possible to see and navigate categories

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -1,53 +1,72 @@
-/**
- * This class is the main view for the application. It is specified in app.js as the
- * "mainView" property. That setting automatically applies the "viewport"
- * plugin causing this view to become the body element (i.e., the viewport).
- *
- * TODO - Replace this content of this view to suite the needs of your application.
- */
-Ext.define('FontPackagesViewer.view.main.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'app-main',
+(function(){
 
-    requires: [
-        'Ext.plugin.Viewport',
-        'Ext.window.MessageBox',
+    /**
+     * generates a content for a tab
+     * @param tabName
+     * @param refName
+     * @param iconCls
+     * @returns {{layout: string, title: *, reference: string, iconCls: *, margin: string, items: [*,*]}}
+     */
+    var getTab = function(tabName, refName, iconCls){
+        return {
+            layout: 'border',
+            title: tabName,
+            reference: refName,
+            iconCls: iconCls,
+            margin: '10 0 0 0',
+            items: [
+                {
+                    title: 'Categories',
+                    reference: refName + '_categories',
+                    width: 300,
+                    region: 'west',
+                    split: true,
+                    xtype: 'grid',
+                    columns: [
+                        {
+                            text: 'Category',
+                            flex: 1,
+                            dataIndex: 'category',
+                            align: 'left'
+                        }
+                    ],
+                    listeners: {
+                        select: 'onCatsGridSelect'
+                    }
+                },
+                {
+                    title: 'Icons',
+                    region: 'center',
+                    reference: refName + '_icons',
+                    layout: 'fit',
+                    scrollable: 'y',
+                    bodyPadding: 20
+                }
+            ]
+        };
+    };
 
-        'FontPackagesViewer.view.main.MainController'
-    ],
+    Ext.define('FontPackagesViewer.view.main.Main', {
+        extend: 'Ext.tab.Panel',
+        xtype: 'app-main',
 
-    controller: 'main',
-    viewModel: true,
+        requires: [
+            'Ext.plugin.Viewport',
+            'Ext.window.MessageBox',
 
-    items: [
-        {
-            title: 'Icon54',
-            reference: 'Icon54all',
-            iconCls: 'x-i54c i54c-you-rock',
-            layout: 'fit',
-            scrollable: 'y',
-            bodyPadding: 20
-        },
-        // {
-        //     title: 'Icon54com',
-        //     reference: 'Icon54com',
-        //     //iconCls: 'fa-home',
-        //     layout: 'fit',
-        //     scrollable: 'y',
-        //     bodyPadding: 20
-        // },
-        {
-            title: 'LinearIcons',
-            reference: 'LinearIcons',
-            iconCls: 'x-li li-diamond3',
-            layout: 'fit',
-            scrollable: 'y',
-            bodyPadding: 20
+            'FontPackagesViewer.view.main.MainController'
+        ],
+
+        controller: 'main',
+        viewModel: true,
+
+        items: [
+            getTab('Icon 54', 'Icon54all', 'x-i54c i54c-you-rock'),
+            getTab('Linear Icons', 'LinearIcons', 'x-li li-diamond3')
+        ],
+
+        listeners: {
+            tabchange: 'onTabChange'
         }
-
-    ],
-
-    listeners: {
-        tabchange: 'onTabChange'
-    }
-});
+    });
+})();
--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -1,9 +1,3 @@
-/**
- * This class is the controller for the main view for the application. It is specified as
- * the "controller" of the Main view class.
- *
- * TODO - Replace this content of this view to suite the needs of your application.
- */
 Ext.define('FontPackagesViewer.view.main.MainController', {
     extend: 'Ext.app.ViewController',
 
@@ -17,12 +11,25 @@ Ext.define('FontPackagesViewer.view.main.MainController', {
         this.loadIcons(this.getView().getActiveTab());
     },
 
+    /**
+     * tab change callback - loads new icons
+     * @param tabpanel
+     * @param newTab
+     * @param oldTab
+     */
     onTabChange: function(tabpanel, newTab, oldTab){
         this.loadIcons(newTab);
     },
 
+    /**
+     * whether or not icons have been loaded for a view
+     */
     iconsLoadStatus: null,
 
+    /**
+     * loads icons from json
+     * @param tab
+     */
     loadIcons: function(tab){
         this.iconsLoadStatus = this.iconsLoadStatus || {};
         if(this.iconsLoadStatus[tab.reference]){
@@ -44,6 +51,30 @@ Ext.define('FontPackagesViewer.view.main.MainController', {
         store.load();
     },
 
+    /**
+     * @private
+     */
+    idGen: null,
+
+    /**
+     * gets an instance of sequential id generator
+     * @returns {null}
+     */
+    getGenerator: function(){
+        if(!this.idGen){
+            this.idGen = Ext.create('Ext.data.identifier.Sequential');
+        }
+        return this.idGen;
+    },
+
+    /**
+     * icons store load callback - prepares categories grid data and icons html
+     * @param store
+     * @param records
+     * @param successful
+     * @param op
+     * @param eOpts
+     */
     onIconsStoreLoad: function(store, records, successful, op, eOpts){
 
         if(!successful){
@@ -54,18 +85,27 @@ Ext.define('FontPackagesViewer.view.main.MainController', {
         store.sort('group', 'ASC');
 
         //split icons into groups
-        var groups = {};
+        var groups = {},
+            html = '',
+            cats = [],
+            idGen = this.me.getGenerator();
+
+
         store.each(function(r){
             var g = r.get('group');
             groups[g] = groups[g] || [];
             groups[g].push(r);
         });
 
-        //generate html
-        var html = '';
+        //generate html and categories list data
         Ext.Array.each(Ext.Object.getKeys(groups), function(group){
-            html += '<div style="display: inline-block; width: 100%;">';
-           html += '<h3 class="iconGroup">' + (group === 'group_name' ? 'All icons' : group) + '</h3>';
+            var groupName = group === 'group_name' ? 'All icons' : group,
+                groupId = 'cat_' + idGen.generate();
+
+            html += '<div id="' + groupId + '" style="display: inline-block; width: 100%;">';
+
+            html += '<h3 class="iconGroup">' + groupName + '</h3>';
+
             Ext.Array.each(groups[group], function(icon){
               html +=
                 '<div class="iconBox">' +
@@ -74,11 +114,35 @@ Ext.define('FontPackagesViewer.view.main.MainController', {
                     '</div>' +
                     '<div class="iconName">' + icon.get('name') + '</div><br/>' +
                 '</div>';
-           });
+            });
+
             html += '</div>';
+
+            cats.push({category: groupName, groupId: groupId});
         });
 
+        //load the cats store
+        this.me.lookupReference(this.tab.reference + '_categories').setStore(
+            Ext.create('Ext.data.Store', {
+                fields: ['category', 'groupId'],
+                data: cats
+            })
+        );
+
         //and push it into tabpanels html
-        this.tab.setHtml(html);
+        this.me.lookupReference(this.tab.reference + '_icons').setHtml(html);
+    },
+
+    /**
+     * cats grid select allback - scrolls cat into view
+     * @param rowModel
+     * @param rec
+     */
+    onCatsGridSelect: function(rowModel, rec){
+        if(!rec){
+            return;
+        }
+
+        Ext.get(rec.get('groupId')).scrollIntoView(this.lookupReference(rowModel.view.grid.reference.replace('_categories', '_icons')).body.dom);
     }
 });
